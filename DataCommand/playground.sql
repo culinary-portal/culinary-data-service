ccab0f8996fb88cff90e4d2166aa12738371f9b8
Kamil <dedzakamil@gmail.com>
2024-11-27 12:05:49 +0100

    substitudes ready

--- DataCommand/playground.sql
+++ DataCommand/playground.sql
@@ -1,4 +1,4 @@
-
+drop table testing;
 
 CREATE TABLE IF NOT EXISTS testing (
 ingredient_id SERIAL PRIMARY KEY,
@@ -7,8 +7,11 @@ fat FLOAT,
 protein FLOAT,
 carbohydrate FLOAT,
 kcal FLOAT,
-is_vegan BOOLEAN,
-is_vegetarian BOOLEAN,
-is_gluten_free BOOLEAN,
-is_lactose_free BOOLEAN
+is_vegan INT,
+is_vegetarian INT,
+is_gluten_free INT,
+is_lactose_free INT,
+is_keto INT
 );
+
+select * from testing where is_vegan = 0 or is_vegetarian = 0 or is_gluten_free = 0 or is_lactose_free = 0;
