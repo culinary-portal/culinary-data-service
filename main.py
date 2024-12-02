8168b58548f62114981f1587e54b56b9c970d1d7
Kamil <dedzakamil@gmail.com>
2024-12-02 12:34:06 +0100

    refactor main

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ import csv
 class Config:
     # Configuration flags for enabling/disabling specific parts of the code
     ENABLE_GENERAL_RECIPE = False
-    ENABLE_RECIPE = False
-    # fetching the ingredients result in having all the data apart from the flags
-    ENABLE_INGREDIENTS = False
+    ENABLE_RECIPE = True
+    ENABLE_INGREDIENTS = False  # fetching the ingredients result in having all the data apart from the flags,
+                                # also keep in mind the 200 bound for API calls
     ENABLE_CONTAINS = False
     ENABLE_SUBSTITUTES = False
+    POST_PROCESSING = False
     MAX_ITERATOR = 300
     MAX_INGREDIENTS = 20
 
@@ -37,7 +38,7 @@ def main():
     contains = TransformContains()
     substitutes = TransformSubstitutes()
 
-    g = general_recipe.post_processing()
+
 
     if Config.ENABLE_SUBSTITUTES:  # creates a new file with all combinations of ids from the substitutes_map
         substitutes = substitutes.transform_data()
@@ -64,26 +65,30 @@ def main():
                     if row:
                         print(f"Writing General Recipe Row for Iterator {iterator}: {row}")
                         general_recipe_file.write(f"{row}\n")
-                # Transform and save the recipe row
-                if Config.ENABLE_RECIPE:
-                    recipe_row = recipe.transform_data(response)
-                    if recipe_row:
-                        print(f"Writing Recipe Row for Iterator {iterator}: {recipe_row}")
-                        recipe_file.write(f"{recipe_row}\n")
+            # Transform and save the recipe row
+        if Config.ENABLE_RECIPE:
+            for iterator in range(Config.MAX_ITERATOR):
+                response = data_service.get_one(iterator)
+                recipe_row = recipe.transform_data(response)
+                if recipe_row:
+                    print(f"Writing Recipe Row for Iterator {iterator}: {recipe_row}")
+                    recipe_file.write(f"{recipe_row}\n")
 
-                if Config.ENABLE_CONTAINS:
-                    max_ingredients_for_recipe = int(data_service.get_number_of_max_ingredients(response)) - 1
-                    for i in range(max_ingredients_for_recipe):
-                        # Transform and save "contains" row
-                        contains_row = contains.transform_data(response, i)
-                        if contains_row:
-                            print(f"Writing Contains Row (Iterator {iterator}, Ingredient {i}): {contains_row}")
-                            contains_file.write(f"{contains_row}\n")
-                        else:
-                            print(f"No valid 'Contains' data for Ingredient {i} in Iterator {iterator}")
-            else:
-                print("Response is none")
+        if Config.ENABLE_CONTAINS:
+            for iterator in range(Config.MAX_ITERATOR):
+                response = data_service.get_one(iterator)
+                max_ingredients_for_recipe = int(data_service.get_number_of_max_ingredients(response)) - 1
+                for i in range(max_ingredients_for_recipe):
+                    # Transform and save "contains" row
+                    contains_row = contains.transform_data(response, i)
+                    if contains_row:
+                        print(f"Writing Contains Row (Iterator {iterator}, Ingredient {i}): {contains_row}")
+                        contains_file.write(f"{contains_row}\n")
+                    else:
+                        print(f"No valid 'Contains' data for Ingredient {i} in Iterator {iterator}")
 
+        if Config.POST_PROCESSING:
+            g = general_recipe.post_processing()
 
 if __name__ == "__main__":
     main()
